ccd29b1565403e9f60bf3ea9c884a31f08dad4ec
arajo-hub <joara9566@naver.com>
2020-06-09 00:02:00 +0900

    AnIncompletePlayer

--- programmers/AnIncompletePlayer.py
+++ programmers/AnIncompletePlayer.py
@@ -5,3 +5,12 @@ def solution(participant, completion):
         if i != j :
             return i
     return participant[-1]
+
+# 아래는 파이썬의 collections라는 모듈을 이용하여 푸는 방법이다.
+# collenctions에 대해 알아둘 것.
+
+import collections
+
+def solution(participant, completion):
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    return list(answer.keys())[0]
