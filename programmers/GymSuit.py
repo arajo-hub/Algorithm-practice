4cdea0611f5c07765cbf7a40fad18c7a60519680
arajo-hub <joara9566@naver.com>
2020-06-11 23:52:14 +0900

    GymSuit

--- programmers/GymSuit.py
+++ programmers/GymSuit.py
@@ -9,23 +9,29 @@ def solution(n, lost, reserve):
         elif i not in lost and i in reserve:
             students[i-1]=2
 
-
     for j in lost:
-        if j==len(students):
-            if students[j-2]==2:
-                students[j-2]=1
-                students[j-1]=1
+        if students[j-1]==1:
+            pass
         else:
-            if students[j-2]==2:
-                students[j-2]=1
-                students[j-1]=1
-            elif students[j]==2:
-                students[j]=1
-                students[j-1]=1
+            if j==len(students):
+                if students[j-2]==2:
+                    students[j-2]=1
+                    students[j-1]=1
+            elif j==1:
+                if students[j]==2:
+                    students[j]=1
+                    students[j-1]=1
+            else:
+                if students[j-2]==2:
+                    students[j-2]=1
+                    students[j-1]=1
+                elif students[j]==2:
+                    students[j]=1
+                    students[j-1]=1
 
     answer=n-students.count(0)
     return answer
 
 # print(solution(5, [2, 4], [1, 3, 5]))
-print(solution(5, [1, 2], [2, 3]))
-print(solution(5, [2, 4, 5], [1, 3]))
+print(solution(6, [1, 3, 5], [2, 4, 6]))
+# print(solution(5, [2, 4, 5], [1, 3]))
