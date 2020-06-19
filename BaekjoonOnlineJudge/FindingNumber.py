ec584afe849c895d21276c9a26644fe26554c907
arajo-hub <joara9566@naver.com>
2020-06-19 23:51:51 +0900

    FindingNumber

---
+++ BaekjoonOnlineJudge/FindingNumber.py
@@ -0,0 +1,9 @@
+import sys
+
+N=int(sys.stdin.readline())
+A=list(map(int, sys.stdin.readline().split()))[:N]
+M=int(sys.stdin.readline())
+
+testA=list(map(int, sys.stdin.readline().split()))[:N]
+
+print(testA)
