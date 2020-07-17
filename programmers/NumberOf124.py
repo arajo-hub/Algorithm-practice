7807cafa4bcdd23c6feebe4ce4ea91509d28f08f
arajo-hub <joara9566@naver.com>
2020-07-18 00:06:41 +0900

    NumberOf124

--- programmers/NumberOf124.py
+++ programmers/NumberOf124.py
@@ -1,13 +1,7 @@
 def solution(n):
-    answer=0
-   
+    answer=''
+    while n>0:
+        n-=1
+        answer='124'[n%3]+answer
+        n//=3
     return answer
-
-print(solution(1))
-print(solution(2))
-print(solution(3))
-print(solution(4))
-print(solution(5))
-print(solution(6))
-print(solution(7))
-print(solution(10))
