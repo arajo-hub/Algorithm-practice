e313b1e515bd071c23dff85fae85e516d6bf1753
arajo-hub <joara9566@naver.com>
2020-07-22 20:05:34 +0900

    No2447

---
+++ BaekjoonOnlineJudge/No2447.py
@@ -0,0 +1,17 @@
+import sys
+
+num = int(input())
+
+def star(i, j):
+    while(i != 0): # i가 0이라면 그냥 *만 프린트
+        if(i % 3 == 1 and j % 3 == 1):
+            sys.stdout.write(' ')
+            return None
+        i = i // 3
+        j = j // 3
+    sys.stdout.write('*')
+
+for i in range(num):
+    for j in range(num):
+            star(i, j)
+    sys.stdout.write('\n')
