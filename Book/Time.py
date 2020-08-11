63e4eb48c20c17895d488414c7ca8e520f3e661e
arajo-hub <joara9566@naver.com>
2020-08-11 22:22:14 +0900

    P.114

---
+++ Book/Time.py
@@ -0,0 +1,12 @@
+import sys
+
+N=int(sys.stdin.readline())
+
+cnt=0
+for hour in range(N+1):
+    for minute in range(60):
+        for second in range(60):
+            if '3' in str(hour)+str(minute)+str(second):
+                cnt+=1
+
+print(cnt)
