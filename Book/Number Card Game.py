c7e034dc85b3f1255e5f112e950d4473624217cb
arajo-hub <joara9566@naver.com>
2020-10-15 19:33:28 +0900

    P.96

--- Book/Number Card Game.py
+++ Book/Number Card Game.py
@@ -1,13 +1,26 @@
+# import sys
+
+# N, M=map(int, sys.stdin.readline().split())
+
+# card=[list(map(int, sys.stdin.readline().split())) for _ in range(N)]
+
+# maxs=0
+
+# for i in range(N):
+#     if min(card[i])>maxs:
+#         maxs=min(card[i])
+
+# print(maxs)
+
+# 2020년 10월 15일 풀이
+
 import sys
 
-N, M=map(int, sys.stdin.readline().split())
+n, m=map(int, sys.stdin.readline().split())
+result=0
+for i in range(n):
+    cardset=list(map(int, sys.stdin.readline().split()))
+    cardset_min=min(cardset)
+    result=max(cardset_min, result)
 
-card=[list(map(int, sys.stdin.readline().split())) for _ in range(N)]
-
-maxs=0
-
-for i in range(N):
-    if min(card[i])>maxs:
-        maxs=min(card[i])
-
-print(maxs)
+print(result)
