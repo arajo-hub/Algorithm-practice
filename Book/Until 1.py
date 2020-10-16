8fc05ce7fbab184b8e63d062809e57ef9eb6174b
arajo-hub <joara9566@naver.com>
2020-10-16 12:26:08 +0900

    P.99

--- Book/Until 1.py
+++ Book/Until 1.py
@@ -1,14 +1,30 @@
+# import sys
+
+# N, K=map(int, sys.stdin.readline().split())
+
+# cnt=0
+
+# while (N!=1):
+#     if N%K!=0:
+#         N-=1
+#     else:
+#         N/=K
+#     cnt+=1
+
+# print(cnt)
+
+# 2020년 10월 16일 풀이
+
 import sys
 
-N, K=map(int, sys.stdin.readline().split())
+n, k=map(int, sys.stdin.readline().split())
 
 cnt=0
-
-while (N!=1):
-    if N%K!=0:
-        N-=1
+while(n!=1):
+    if n%k==0:
+        n/=k
     else:
-        N/=K
+        n-=1
     cnt+=1
 
 print(cnt)
