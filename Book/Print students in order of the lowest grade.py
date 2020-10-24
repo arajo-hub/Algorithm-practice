8cb22d6c29be0ddb95a9498b3a3490e803e674da
arajo-hub <joara9566@naver.com>
2020-10-24 17:26:05 +0900

    P.180

--- Book/Print students in order of the lowest grade.py
+++ Book/Print students in order of the lowest grade.py
@@ -1,30 +1,46 @@
-# list를 이용한 풀이
+# # list를 이용한 풀이
+
+# import sys
+
+# students=[]
+
+# for i in range(int(sys.stdin.readline())):
+#     person=list(sys.stdin.readline().split())
+#     person[0], person[1]=int(person[1]), person[0]
+#     students.append(person)
+
+# students=sorted(students)
+
+# for j in students:
+#     print(j[1], end=' ')
+
+# # dictionary를 이용한 풀이
+
+# students={}
+
+# for i in range(int(sys.stdin.readline())):
+#     person=list(sys.stdin.readline().split())
+#     students[person[0]]=int(person[1])
+
+# for key, value in sorted(students.items(), key=lambda students:students[1]):
+#     print(key, end=' ')
+
+# # dictionary형에서 key를 기준으로 정렬하고 싶을 땐,
+# # sorted(dict.items(), key=lambda x:x[1]) (내림차순)
+# # sorted(dict.items(), reverse=True, key=lambda x:x[1]) (오름차순)
+
+# 2020년 10월 24일 풀이
 
 import sys
 
-students=[]
+n=int(sys.stdin.readline())
+student=[]
 
-for i in range(int(sys.stdin.readline())):
-    person=list(sys.stdin.readline().split())
-    person[0], person[1]=int(person[1]), person[0]
-    students.append(person)
+for i in range(n):
+    name, score=sys.stdin.readline().split()
+    student.append((int(score), name))
 
-students=sorted(students)
+student.sort()
 
-for j in students:
-    print(j[1], end=' ')
-
-# dictionary를 이용한 풀이
-
-students={}
-
-for i in range(int(sys.stdin.readline())):
-    person=list(sys.stdin.readline().split())
-    students[person[0]]=int(person[1])
-
-for key, value in sorted(students.items(), key=lambda students:students[1]):
-    print(key, end=' ')
-
-# dictionary형에서 key를 기준으로 정렬하고 싶을 땐,
-# sorted(dict.items(), key=lambda x:x[1]) (내림차순)
-# sorted(dict.items(), reverse=True, key=lambda x:x[1]) (오름차순)
+for j in student:
+    print(j[1], end=" ")
