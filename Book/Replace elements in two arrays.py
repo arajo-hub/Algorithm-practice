49cf9a31b06d0ccab46086d4eac2ed7becfde0af
arajo-hub <joara9566@naver.com>
2020-10-25 17:05:25 +0900

    P.182

--- Book/Replace elements in two arrays.py
+++ Book/Replace elements in two arrays.py
@@ -1,14 +1,34 @@
+# import sys
+
+# N, K=map(int, sys.stdin.readline().split())
+
+# array1=sorted(list(map(int, sys.stdin.readline().split())))
+# array2=sorted(list(map(int, sys.stdin.readline().split())), reverse=True)
+
+# for i in range(K):
+#     if array1[i]<array2[i]:
+#         array1[i], array2[i]=array2[i], array1[i]
+#     else:
+#         break
+
+# print(sum(array1))
+
+# 2020년 10월 25일 풀이
+
 import sys
 
-N, K=map(int, sys.stdin.readline().split())
+n, k=map(int, sys.stdin.readline().split())
 
-array1=sorted(list(map(int, sys.stdin.readline().split())))
-array2=sorted(list(map(int, sys.stdin.readline().split())), reverse=True)
+arrayA=list(map(int, sys.stdin.readline().split()))
+arrayB=list(map(int, sys.stdin.readline().split()))
 
-for i in range(K):
-    if array1[i]<array2[i]:
-        array1[i], array2[i]=array2[i], array1[i]
+arrayA.sort()
+arrayB.sort(reverse=True)
+
+for i in range(k):
+    if arrayA[i]<arrayB[i]:
+        arrayA[i], arrayB[i]=arrayB[i],arrayA[i]
     else:
         break
 
-print(sum(array1))
+print(sum(arrayA))
