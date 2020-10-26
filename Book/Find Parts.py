a5e29860096426d08d63d1d9d285013eabf076c5
arajo-hub <joara9566@naver.com>
2020-10-26 09:22:16 +0900

    P.197

--- Book/Find Parts.py
+++ Book/Find Parts.py
@@ -1,60 +1,83 @@
+# import sys
+
+# # 가장 단순한 방법
+
+# N=int(sys.stdin.readline())
+# parts=sorted(list(map(int, sys.stdin.readline().split())))
+
+# M=int(sys.stdin.readline())
+# customer=list(map(int, sys.stdin.readline().split()))
+
+# for i in customer:
+#     if i in parts:
+#         print('yes', end=' ')
+#     else:
+#         print('no', end=' ')
+
+# # 이진 탐색 알고리즘 이용하는 방법
+
+# def binarySearch(array, target, start, end):
+#     while start<=end:
+#         mid=(start+end)//2
+#         if array[mid]==target:
+#             return mid
+#         elif array[mid]>target:
+#             end=mid-1
+#         else:
+#             start=mid+1
+#     return None
+
+# N=int(sys.stdin.readline())
+# parts=sorted(list(map(int, sys.stdin.readline().split())))
+
+# M=int(sys.stdin.readline())
+# customer=list(map(int, sys.stdin.readline().split()))
+
+# result=[]
+
+# for i in customer:
+#     result=binarySearch(parts, i, 0, N-1)
+#     if result!=None:
+#         print('yes', end=' ')
+#     else:
+#         print('no', end=' ')
+
+# # 계수 정렬을 이용하는 방법
+
+# N=int(sys.stdin.readline())
+# parts=[0]*1000000
+
+# for i in map(int, sys.stdin.readline().split()):
+#     parts[i]=1
+
+# M=int(sys.stdin.readline())
+# customer=list(map(int, sys.stdin.readline().split()))
+
+# for i in customer:
+#     if parts[i]==1:
+#         print('yes', end=' ')
+#     else:
+#         print('no', end=' ')
+
+# 2020년 10월 26일 풀이
+
 import sys
 
-# 가장 단순한 방법
+n=int(sys.stdin.readline())
 
-N=int(sys.stdin.readline())
-parts=sorted(list(map(int, sys.stdin.readline().split())))
+parts=list(map(int, sys.stdin.readline().split()))
 
-M=int(sys.stdin.readline())
+m=int(sys.stdin.readline())
+
 customer=list(map(int, sys.stdin.readline().split()))
 
-for i in customer:
-    if i in parts:
-        print('yes', end=' ')
+parts.sort()
+
+answer=[]
+for i in range(m):
+    if customer[i] in parts:
+        answer.append("yes")
     else:
-        print('no', end=' ')
+        answer.append("no")
 
-# 이진 탐색 알고리즘 이용하는 방법
-
-def binarySearch(array, target, start, end):
-    while start<=end:
-        mid=(start+end)//2
-        if array[mid]==target:
-            return mid
-        elif array[mid]>target:
-            end=mid-1
-        else:
-            start=mid+1
-    return None
-
-N=int(sys.stdin.readline())
-parts=sorted(list(map(int, sys.stdin.readline().split())))
-
-M=int(sys.stdin.readline())
-customer=list(map(int, sys.stdin.readline().split()))
-
-result=[]
-
-for i in customer:
-    result=binarySearch(parts, i, 0, N-1)
-    if result!=None:
-        print('yes', end=' ')
-    else:
-        print('no', end=' ')
-
-# 계수 정렬을 이용하는 방법
-
-N=int(sys.stdin.readline())
-parts=[0]*1000000
-
-for i in map(int, sys.stdin.readline().split()):
-    parts[i]=1
-
-M=int(sys.stdin.readline())
-customer=list(map(int, sys.stdin.readline().split()))
-
-for i in customer:
-    if parts[i]==1:
-        print('yes', end=' ')
-    else:
-        print('no', end=' ')
+print(*answer)
