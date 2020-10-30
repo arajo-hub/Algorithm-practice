d3e0e80546fd9b7058034e6c6c48b03c667437cd
arajo-hub <joara9566@naver.com>
2020-10-30 20:07:11 +0900

    P.220

--- Book/Ant Warrior.py
+++ Book/Ant Warrior.py
@@ -1,16 +1,31 @@
+# import sys
+
+# N=int(sys.stdin.readline())
+
+# warehouse=list(map(int, sys.stdin.readline().split()))
+
+# dp=[0]*100
+
+# dp[0]=warehouse[0]
+
+# dp[1]=max(warehouse[0], warehouse[1])
+
+# for i in range(2, N):
+#     dp[i]=max(dp[i-1], dp[i-2]+warehouse[i])
+
+# print(dp[N-1])
+
+# 2020년 10월 30일 풀이
+
 import sys
 
-N=int(sys.stdin.readline())
-
+n=int(sys.stdin.readline())
 warehouse=list(map(int, sys.stdin.readline().split()))
-
 dp=[0]*100
 
 dp[0]=warehouse[0]
-
 dp[1]=max(warehouse[0], warehouse[1])
-
-for i in range(2, N):
+for i in range(2, n):
     dp[i]=max(dp[i-1], dp[i-2]+warehouse[i])
 
-print(dp[N-1])
+print(dp[n-1])
