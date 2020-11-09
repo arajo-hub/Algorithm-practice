ede32e7be4731d7892a3ed1c877a9ee7512192a3
arajo-hub <joara9566@naver.com>
2020-11-09 09:09:26 +0900

    P.312

--- Book/Multiply Or Add.py
+++ Book/Multiply Or Add.py
@@ -1,9 +1,25 @@
+# import sys
+
+# S=sys.stdin.readline().strip('\n')
+
+# result=0
+# for num in S:
+#     if int(num)<=1 or result<=1:
+#         result+=int(num)
+#     else:
+#         result*=int(num)
+
+# print(result)
+
+# 2020년 11월 9일 풀이
+
 import sys
 
-S=sys.stdin.readline().strip('\n')
+s=sys.stdin.readline().strip('\n')
 
 result=0
-for num in S:
+
+for num in s:
     if int(num)<=1 or result<=1:
         result+=int(num)
     else:
