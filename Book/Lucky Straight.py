81d8613558234dc1d8f32edb82e808d8db5af341
arajo-hub <joara9566@naver.com>
2020-11-15 13:15:43 +0900

    P.321

--- Book/Lucky Straight.py
+++ Book/Lucky Straight.py
@@ -1,8 +1,22 @@
+# import sys
+
+# N=list(map(int, sys.stdin.readline().strip('\n')))
+
+# if sum(N[:len(N)//2])==sum(N[len(N)//2:]):
+#     print("LUCKY")
+# else:
+#     print("READY")
+
+# 2020년 11월 15일 풀이
+
 import sys
 
-N=list(map(int, sys.stdin.readline().strip('\n')))
+n=list(map(int, sys.stdin.readline().strip('\n')))
 
-if sum(N[:len(N)//2])==sum(N[len(N)//2:]):
+left=n[:len(n)//2]
+right=n[len(n)//2:]
+
+if sum(left)==sum(right):
     print("LUCKY")
 else:
     print("READY")
