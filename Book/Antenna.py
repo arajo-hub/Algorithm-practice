95e61f69c72d55687dcc3e9e759e1d1fe8eaf37f
arajo-hub <joara9566@naver.com>
2020-11-30 08:43:02 +0900

    P.360

--- Book/Antenna.py
+++ Book/Antenna.py
@@ -1,7 +1,18 @@
+# import sys
+
+# N=int(sys.stdin.readline()) # 집의 수
+# distance=list(map(int, sys.stdin.readline().split()))
+# distance.sort()
+
+# print(distance[(N-1)//2])
+
+# 2020년 11월 30일 풀이
+
 import sys
 
-N=int(sys.stdin.readline()) # 집의 수
-distance=list(map(int, sys.stdin.readline().split()))
-distance.sort()
+n=int(sys.stdin.readline())
 
-print(distance[(N-1)//2])
+house=list(map(int, sys.stdin.readline().split()))
+
+house.sort()
+print(house[(n-1)//2])
