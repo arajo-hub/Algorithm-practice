f3eefef0aca0e0ee54797b456932e0d31e43a342
arajo-hub <joara9566@naver.com>
2020-12-02 10:17:32 +0900

    P.363

--- Book/Sorting cards.py
+++ Book/Sorting cards.py
@@ -1,20 +1,42 @@
+# import heapq
+# import sys
+
+# N=int(sys.stdin.readline())
+
+# heap=[]
+# for i in range(N):
+#     data=int(sys.stdin.readline())
+#     heapq.heappush(heap, data)
+
+# result=0
+
+# while len(heap)!=1:
+#     one=heapq.heappop(heap)
+#     two=heapq.heappop(heap)
+#     sum_value=one+two
+#     result+=sum_value
+#     heapq.heappush(heap, sum_value)
+
+# print(result)
+
+# 2020년 12월 2일 풀이
+
 import heapq
 import sys
 
-N=int(sys.stdin.readline())
+n=int(sys.stdin.readline()) # 카드묶음 개수
 
 heap=[]
-for i in range(N):
-    data=int(sys.stdin.readline())
-    heapq.heappush(heap, data)
+for i in range(n):
+    heapq.heappush(heap, int(sys.stdin.readline()))
 
-result=0
+total=0
 
 while len(heap)!=1:
     one=heapq.heappop(heap)
     two=heapq.heappop(heap)
-    sum_value=one+two
-    result+=sum_value
-    heapq.heappush(heap, sum_value)
+    sumVal=one+two
+    total+=sumVal
+    heapq.heappush(heap, sumVal)
 
-print(result)
+print(total)
