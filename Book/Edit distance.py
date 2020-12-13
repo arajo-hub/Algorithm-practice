85616604788ff63200ba08f5c85e4845ecd8c847
arajo-hub <joara9566@naver.com>
2020-12-13 14:52:35 +0900

    P.382

--- Book/Edit distance.py
+++ Book/Edit distance.py
@@ -1,28 +1,56 @@
-def edit_dist(str1, str2):
-    n=len(str1)
-    m=len(str2)
+# def edit_dist(str1, str2):
+#     n=len(str1)
+#     m=len(str2)
 
-    # 다이나믹 프로그래밍을 위한 2차원 DP 테이블 초기화
-    # 세로는 변경전 문자열, 가로는 변경후 문자열
-    # dp의 대각선을 따라서 몇 번 바꾸어야 하는지 표시된다.
-    dp=[[0]*(m+1) for _ in range(n+1)]
+#     # 다이나믹 프로그래밍을 위한 2차원 DP 테이블 초기화
+#     # 세로는 변경전 문자열, 가로는 변경후 문자열
+#     # dp의 대각선을 따라서 몇 번 바꾸어야 하는지 표시된다.
+#     dp=[[0]*(m+1) for _ in range(n+1)]
+
+#     for i in range(1, n+1):
+#         dp[i][0]=i
+
+#     for j in range(1, m+1):
+#         dp[0][j]=j # 바뀔 글자수만큼 [0]에 표시
+
+#     for i in range(1, n+1):
+#         for j in range(1, m+1):
+#             if str1[i-1]==str2[j-1]: # 문자가 같다면
+#                 dp[i][j]=dp[i-1][j-1] # 변화없이 그대로
+#             else: # 문자가 다르다면 왼쪽값, 위쪽값, 대각선값 중 가장 작은 값에 +1을 해준다.
+#                 dp[i][j]=1+min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1])
+
+#     return dp[n][m]
+
+# str1=input()
+# str2=input()
+
+# print(edit_dist(str1, str2))
+
+# 2020년 12월 13일 풀이
+
+def solution(a, b): # 두 문자열 a, b
+    n=len(a)
+    m=len(b)
+    dp=[[0]*(m+1) for _ in range(n+1)] # 가로 m, 세로 n
 
     for i in range(1, n+1):
         dp[i][0]=i
-
+    
     for j in range(1, m+1):
-        dp[0][j]=j # 바뀔 글자수만큼 [0]에 표시
-
-    for i in range(1, n+1):
-        for j in range(1, m+1):
-            if str1[i-1]==str2[j-1]: # 문자가 같다면
-                dp[i][j]=dp[i-1][j-1] # 변화없이 그대로
-            else: # 문자가 다르다면 왼쪽값, 위쪽값, 대각선값 중 가장 작은 값에 +1을 해준다.
-                dp[i][j]=1+min(dp[i][j-1], dp[i-1][j], dp[i-1][j-1])
-
+        dp[0][j]=j
+    
+    for k in range(1, n+1):
+        for l in range(1, m+1):
+            if a[k-1]==b[l-1]:
+                dp[k][l]=dp[k-1][l-1]
+            else:
+                dp[k][l]=1+min(dp[k][l-1], dp[k-1][l], dp[k-1][l-1])
     return dp[n][m]
 
-str1=input()
-str2=input()
+import sys
 
-print(edit_dist(str1, str2))
+a=sys.stdin.readline().strip('\n')
+b=sys.stdin.readline().strip('\n')
+
+print(solution(a, b))
