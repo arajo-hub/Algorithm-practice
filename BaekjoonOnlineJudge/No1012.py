4e72885e82ca019345d1f60dad062944a772ef3f
arajo-hub <joara9566@naver.com>
2021-03-03 17:46:47 +0900

    No1012

--- BaekjoonOnlineJudge/No1012.py
+++ BaekjoonOnlineJudge/No1012.py
@@ -1,13 +1,31 @@
-from sys import stdin
-read=stdin.readline
+t = int(input())
 
-for i in range(int(read())):
-    garo, sero, cnt=map(int, read().split())
-    matrix=[[0]*(garo) for i in range(sero)]
+dx = [1, -1, 0, 0]
+dy = [0, 0, -1, 1]
 
-# for _ in range(cnt):
-#     link = list(map(int, read().split()))
-#     matrix[link[0]][link[1]] = 1
-
-for j in range(len(matrix)):
-    print(matrix[j])
+def bfs(x, y):
+    queue = [[x, y]]
+    while queue:
+        a, b = queue[0][0], queue[0][1]
+        del queue[0]
+        for i in range(4):
+            q = a + dx[i]
+            w = b + dy[i]
+            if 0 <= q < n and 0 <= w < m and s[q][w] == 1:
+                s[q][w] = 0
+                queue.append([q, w])
+                
+for i in range(t):
+    m, n, k = map(int, input().split())
+    s = [[0] * m for i in range(n)]
+    cnt = 0
+    for j in range(k):
+        a, b = map(int, input().split())
+        s[b][a] = 1
+    for q in range(n):
+        for w in range(m):
+            if s[q][w] == 1:
+                bfs(q, w)
+                s[q][w] = 0
+                cnt += 1
+    print(cnt)
