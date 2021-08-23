3926a7ef9d527071ca0d2b3b781c563732893877
arajo-hub <joara9566@naver.com>
2021-08-23 23:55:13 +0900

    CreatingJadenCaseString

---
+++ programmers/CreatingJadenCaseString.py
@@ -0,0 +1,8 @@
+def solution(s):
+    answer=''
+    s=s.split(' ')
+    for i in range(len(s)):
+        if not s[i][0].isdecimal():
+            s[i]=s[i][0].upper()+s[i][1:].lower()
+    answer=' '.join(s)
+    return answer
